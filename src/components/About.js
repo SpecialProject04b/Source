dbe87761d9479bdc91e206e2533198bdab0a3a63
Jingquan <2101268@sit.singaporetech.edu.sg>
2023-07-25 18:37:51 +0800

    asda

--- src/components/About.js
+++ src/components/About.js
@@ -5,33 +5,41 @@ import Header from './Header';
 import Footer from './Footer';
 
 import React, { Component } from 'react';
+
 export default class About extends Component {
+  state = {questions: null,
+          displayQuestions: false,
+          index: 0}
+
+  click = (i) => {
+    this.setState({
+      displayQuestions: !this.state.displayQuestions,
+      index: i
+   });
+}
+
   render() {
     let resumeData = this.props.resumeData;
+
+    if ( this.state.displayQuestions ) {
+      console.log("inside")
+      return (
+        <div style={{height:"100vh", backgroundColor:"black",}}>
+          <img src={data[this.state.index].src} style={{height:"80vh", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} alt=""onClick={this.click}></img>
+        </div>
+      )
+    }
     return (
       <div id="page" style={{display:"flex", flexDirection:"column", justifyContent:"space-between", height:"100vh"}}>
         <div><Header resumeData={resumeData}/></div>
         <div><h1 style={{color:"white", paddingLeft:"30px"}}>Collection Gallery</h1></div>
         <div className="flexbox gallery">
-            <PhotoProvider  speed={() => 300} 
-                            easing={(type) => (type === 2 ? 'cubic-bezier(.17,.67,.83,.67)' : 'cubic-bezier(0.34, 1.56, 0.64, 1)')}
-                            toolbarRender={({ onScale, scale }) => {
-                              return (
-                                <>
-                                  <svg className="PhotoView-Slider__toolbarIcon" onClick={() => onScale(scale + 1)} />
-                                  <svg className="PhotoView-Slider__toolbarIcon" onClick={() => onScale(scale - 1)} />
-                                </>
-                              );
-                            }}>
             {data.map((item, index) => (
-              <PhotoView key={index} src={item.src}>
-                <img src={item.src} style={{ objectFit: 'cover', height:200, width:200 }} alt="" />
-              </PhotoView>
-            ))} 
-            </PhotoProvider>
+                <img src={item.src} style={{ objectFit: 'cover', height:200, width:200 }} alt="" onClick={() => this.click(index)}/>))}
         </div>
         <div><Footer resumeData={resumeData}/></div>
       </div>
+      
     );
   }
 }
