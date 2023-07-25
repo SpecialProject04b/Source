cb51dd89912a4c31276c50f8fe29d386f836545e
Jingquan <2101268@sit.singaporetech.edu.sg>
2023-07-25 21:58:04 +0800

    asda

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { data } from "./constant/data";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
 import Header from './Header';
 import Footer from './Footer';
 
@@ -24,9 +25,24 @@ export default class About extends Component {
     if ( this.state.displayQuestions ) {
       console.log("inside")
       return (
-        <div style={{height:"100vh", backgroundColor:"black",}}>
-          <img src={data[this.state.index].src} style={{height:"80vh", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} alt=""onClick={this.click}></img>
+        <div id="page" style={{display:"flex", flexDirection:"column", justifyContent:"space-between", height:"100vh"}}>
+          <div id="overlay" style={{zIndex:"999", position:"absolute", top:"0", left: "0", width: "100%", height:"100vh", backgroundColor:"rgba(0,0,0,0.8)"}}>
+            <div className="toolbars" style={{width:"100%", backgroundColor:"black", height:"46px", display:"flex", flexDirection:"row", justifyContent:"flex-end"}}>
+              <FontAwesomeIcon icon={faPlus} style={{width:"46px", fontSize:"x-large", marginTop:"auto", marginBottom:"auto"}}/>
+              <FontAwesomeIcon icon={faMinus} style={{width:"46px", fontSize:"x-large", marginTop:"auto", marginBottom:"auto"}}/>
+              <FontAwesomeIcon icon={faX} onClick={this.click} style={{width:"46px", fontSize:"x-large", marginTop:"auto", marginBottom:"auto"}}/>
+            </div>  
+            <img src={data[this.state.index].src} style={{height:"80vh", transform: "translate(40%, 10%)"}} alt=""></img>
+          </div>
+          <div><Header resumeData={resumeData}/></div>
+          <div><h1 style={{color:"white", paddingLeft:"30px"}}>Collection Gallery</h1></div>
+          <div className="flexbox gallery">
+              {data.map((item, index) => (
+                  <img src={item.src} style={{ objectFit: 'cover', height:200, width:200 }} alt="" onClick={() => this.click(index)}/>))}
+          </div>
+          <div><Footer resumeData={resumeData}/></div>
         </div>
+        
       )
     }
     return (
