6bc214a28b5b4fdc191c64fd6feeecdaa22af91e
alexNeoKs <neoalexanderks@gmail.com>
2023-07-25 18:57:22 +0800

    Homepage Design

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
+
+    
+
+
     return (
       <React.Fragment>
         {/* <header id="home"> */}
-          <nav id="nav-wrap" style={{backgroundColor:"black"}}>
+          <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
             <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
               <li className="current"><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/resume">Resume</Link></li>
-              <li><Link to="/portfolio">Works</Link></li>
-              <li><Link to="/testimonials">Testimonials</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
+              <li><Link to="/about">Our Collection</Link></li>
+              <li><Link to="/resume">Thomson</Link></li>
+              <li><Link to="/portfolio">NewsLetter</Link></li>
+              {/* <li><Link to="/portfolio">Works</Link></li>
+              <li><Link to="/testimonials">Testimonials</Link></li> */}
+              <li><Link to="/contact">Contact Us</Link></li>
             </ul>
           </nav>
         {/* </header> */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+import animation from '../animation.css'
+
 export default class Home extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -9,38 +11,107 @@ export default class Home extends Component {
     const backgroundStyles = {
       zIndex: -1,
       position: 'absolute',
-      top: '70%',
-      left: '50%',
-      transform: 'translate(-50%, -70%)',
-      width: '100%'
+      top: '10%',
+      // left: '50%',
+      // transform: 'translate(-50%, -70%)',
+      width: '100%',
+      animation: 'floatAnimation 3s infinite'
     };
 
-    const homepageText = {
-      display:"flex", 
-      flexDirection:"column", 
-      justifyContent:"space-between", 
-      height:"100vh", 
-      overflowY:"hidden"
+    const homepageBox = {
+      zIndex: 2,
+      position: 'absolute',
+      top: '55%',
+      left: '1%',
+      // transform: 'translate(-50%, -70%)',
+      width: '350px',
+      height:'310px',
+      textAlign: 'left',
+      backgroundColor: 'black',
+      color:'white',
+      
+      
     };
 
-    const bannerTextH1 = {
-      fontSize: '72px',
+    const bannerText ={
+      zIndex: -2,
+      position: 'absolute',
+      top: '10%',
+      left: '5%',
       textAlign: 'center',
-      marginTop: '10%'
-    };
+      color: 'black',
+      letterSpacing: '55px',
+      fontSize: '64px',
+      lineHeight: '2.6'
+
+    }
+
+    const bannerTextName ={
+      zIndex: 2,
+      position: 'absolute',
+      top: '60%',
+      left: '30%',
+      
+      color: 'white',
+      
+      fontSize: '96px',
+      textShadow: `
+      -1px -1px 0 black,
+       1px -1px 0 black,
+      -1px 1px 0 black,
+       1px 1px 0 black`,
+
+    }
+
+    const spanText ={
+      color: 'white',
+      textShadow: `
+      -1px -1px 0 black,
+       1px -1px 0 black,
+      -1px 1px 0 black,
+       1px 1px 0 black`,
+
+    }
 
     return (
       <React.Fragment>
+
           <div id="3dBackground" style={backgroundStyles}>
             <iframe src="https://app.vectary.com/p/0QIOv3TQQwt0Zd3xry96Z4" frameBorder="0" width="100%" height="650"></iframe>
           </div>
 
-          <div id="page" style={homepageText}>
+          <div id="page">
+            <div><Header resumeData={resumeData}/></div>
+            <div style={homepageBox}>
+              <h2 style={{marginTop:'50px',fontSize:'24px',marginLeft:'20px',lineHeight:'1.5',width:'300px'}}>
+                Welcome to 
+                Mitchell library's collection
+              </h2>
+              <p style={{fontSize:'16px',marginLeft:'20px', lineHeight:'1.6',width:'300px'}}>
+                Find out more about Alexander "Greek" Thomson's works 
+                and his contribution to glasgow.
+              </p>
+              <button style={{marginLeft:'20px',width:'300px'}}>
+                Find Out More
+              </button>
+          </div>
 
-              <div><Header resumeData={resumeData}/></div>
+          <p style={bannerText}>
+            VISIONARY
+            <span style={spanText}>"GREEK"</span><br/>
+            ARCHITECT
+          
+            
+          </p>
+          <p style={bannerTextName}>
+            ALEXANDER THOMSON
+          </p>
+            
+
+              {/* <div><Header resumeData={resumeData}/></div>
               <div className="row banner">
                 <div className="banner-text">
-                  <h1 className="responsive-headline" style={bannerTextH1}>{resumeData.name}</h1>
+                  <h1 className="responsive-headline" style={{fontSize:'40px',textAlign: 'center',marginTop: '10%'}}>{resumeData.name}</h1>
                   <h3 style={{  fontFamily: 'sans-serif ' ,textAlign: 'center',marginTop: '30%'}}>{resumeData.roleDescription}</h3>
                   <hr />
                   <ul className="social">
@@ -60,7 +131,7 @@ export default class Home extends Component {
                 <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
               </p>
 
-              <div><Footer resumeData={resumeData}/></div>
+              <div><Footer resumeData={resumeData}/></div> */}
 
           </div>
 
