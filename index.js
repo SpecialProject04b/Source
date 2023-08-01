7628581fd118b6acc2a4bf6418ff0e2dfeb1de36
Zepse Wolf <seekaicong@gmail.com>
2023-08-01 11:51:06 +0100

    Reduce file level
    Reduce file level

--- index.js
+++ index.js
@@ -55,10 +55,6 @@ app.get('/scrape/:page', async (req, res) => {
 });
 
 
-
-
-
-
 // Middleware to parse JSON and URL-encoded data in the request body
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -68,7 +64,7 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 // next(); // Call next() to pass control to the next middleware in the chain
 // });
 
-// app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, './build')));
 
 mongoose.connect("mongodb+srv://specialproject:specialproject123@cluster0.v0joqli.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
